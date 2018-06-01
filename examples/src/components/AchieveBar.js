2dbb11c954242edacc0cea6945b1708bac48d056
zxcv551133 <zxcv551133@gmail.com>
2018-06-02 01:41:09 +0900

    Merge branch 'feature/Products' into develop

--- examples/src/components/AchieveBar.js
+++ examples/src/components/AchieveBar.js
@@ -3,17 +3,33 @@ import './AchieveBar.css'
 
 class AchieveBar extends Component {
   state = {
-    rate: 0
+    achieveRate: 0,
+    achievedState: 0
   }
 
   // aim에서 가장 큰 값을 구하고 setstate함
   getRate() {
     const { aim, current } = this.props
+    let maxVal = aim[aim.length - 1]
+    if(maxVal <= current) {
+      this.setState ({
+        achieveRate: 1
+      })
+    } else {
+      this.setState ({
+        achieveRate: current/maxVal
+      })
+    }
   }
 
-  
-  makeBar() {
-    
+  // 몇단계 목표금액 %
+  makeBar(rate) {
+    return (
+      <div className='Bar'>
+        <div className='graduation'>
+        </div>
+      </div>      
+    )
   }
 
   render () {
@@ -22,7 +38,7 @@ class AchieveBar extends Component {
     return (
       // bar 의 aim 은 flex를 이용해 가로로 늘어지게한다.
       <div className='Bar'>
-
+        
       </div>
     )
   }
